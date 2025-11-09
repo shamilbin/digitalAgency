5989222c1b30a98db80cf9669ac7f922fbcb7a6f
shamilbin <shamiltk029@gmail.com>
2025-11-09 23:50:50 +0530

    minor update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='bg-slate-50 '>
+    <div className='bg-slate-50 dark:bg-gray-950 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40 '>
+            {/* Footer top  */}
+        <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
+            <div>
+                <img src={assets.logo} className='w-32 sm:w-44' alt="" />
+            </div>
+            <div>
+
+            </div>
+        </div>
       
     </div>
   )
